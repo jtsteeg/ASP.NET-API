bba8eec4a9948d1e2f12ce650e67220ec19b5279
Jacob Steeg <jacob.t.steeg@gmail.com>
2021-10-26 10:32:15 -0500

    updates startup file to use in-memory database

--- WebApi1/Startup.cs
+++ WebApi1/Startup.cs
@@ -11,6 +11,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApi1.Models;
 
 namespace WebApi1
 {
@@ -28,10 +30,13 @@ namespace WebApi1
         {
 
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebApi1", Version = "v1" });
-            });
+
+            services.AddDbContext<TodoContext>(opt =>
+                                   opt.UseInMemoryDatabase("TodoList"));
+            //services.AddSwaggerGen(c =>
+            //{
+            //    c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebApi1", Version = "v1" });
+            //});
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,8 +45,8 @@ namespace WebApi1
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebApi1 v1"));
+            //    app.UseSwagger();
+            //    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebApi1 v1"));
             }
 
             app.UseHttpsRedirection();
